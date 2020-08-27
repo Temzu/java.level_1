ab1ce23669a80032099b209b5ae7b26d6197bb38
Temzu <40913z@mail.ru>
2020-08-27 22:53:32 +0300

    Second task
    Initialize all variables, which learned

--- lesson_1/src/ru/geekbrains/home_work/Main.java
+++ lesson_1/src/ru/geekbrains/home_work/Main.java
@@ -2,6 +2,52 @@ package ru.geekbrains.home_work;
 
 public class Main {
     public static void main(String[] args) {
-        
+        // целочисленные
+        byte b = setByte((byte) 1);
+        short s = setShort((short) 2);
+        int integer = setInt(3);
+        long l = setLong(4);
+
+        // с плавающей точкой
+        float f = setFloat((float) 1.2);
+        double d = setDouble(12.3);
+
+        // символьный
+        char c = setChar('a');
+
+        // логический
+        boolean logic = setBoolean(true);
+    }
+
+    static byte setByte(Byte b) {
+        return b;
+    }
+
+    static short setShort(Short s) {
+        return s;
+    }
+
+    static int setInt(int integer) {
+        return integer;
+    }
+
+    static long setLong(long l) {
+        return l;
+    }
+
+    static float setFloat(Float f) {
+        return f;
+    }
+
+    static double setDouble(double d) {
+        return d;
+    }
+
+    static char setChar(char c) {
+        return c;
+    }
+
+    static boolean setBoolean(boolean logic) {
+        return logic;
     }
 }
