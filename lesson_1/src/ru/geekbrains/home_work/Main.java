e758336b463206dd202b24d5aa1ca2a66739a92d
Temzu <40913z@mail.ru>
2020-08-31 21:11:57 +0300

    Create project for 2th lesson

--- lesson_1/src/ru/geekbrains/home_work/Main.java
+++ lesson_1/src/ru/geekbrains/home_work/Main.java
@@ -2,52 +2,98 @@ package ru.geekbrains.home_work;
 
 public class Main {
     public static void main(String[] args) {
-        // целочисленные
-        byte b = setByte((byte) 1);
-        short s = setShort((short) 2);
-        int integer = setInt(3);
-        long l = setLong(4);
+        char d = 'c';
+        int b = 2;
+        float a = (b * 2.4f) / 2;
+        System.out.println(++d);
+        switch (a)
 
-        // с плавающей точкой
-        float f = setFloat((float) 1.2);
-        double d = setDouble(12.3);
 
-        // символьный
-        char c = setChar('a');
+        System.out.println(calculate(1, 2, 3, 4));
 
-        // логический
-        boolean logic = setBoolean(true);
+        System.out.println(isBetween10And20(2, 18));
+
+        positiveOrNegative(5);
+
+        System.out.println(isNegativeNumber(-1));
+
+        outputGreeting("Artem");
+
+        isLeapYearOrNot(1600);
+
+        outputAllVariables((byte) 12, (short) 13, 125, 2312312312L, 12123.12f, 123213213.123123, 'A', true);
     }
 
-    static byte setByte(Byte b) {
-        return b;
+    /**
+     * Outputs all kinds of simple variables to the console.
+     */
+    static void outputAllVariables(byte b, short s, int integer, long l, float f, double d, char c, boolean logic) {
+        System.out.println("Переменная типа byte: " + b);
+        System.out.println("Переменная типа short: " + s);
+        System.out.println("Переменная типа int: " + integer);
+        System.out.println("Переменная типа long: " + l);
+        System.out.println("Переменная типа float: " + f);
+        System.out.println("Переменная типа double: " + d);
+        System.out.println("Переменная типа char: " + c);
+        System.out.println("Переменная типа boolean: " + logic);
     }
 
-    static short setShort(Short s) {
-        return s;
+    /**
+     * Outputs to the console what year: leap year or not.
+     */
+    static void isLeapYearOrNot(int year) {
+        if (((year % 4 == 0) && (year % 100 != 0)) || ((year % 4 == 0) && (year % 400 == 0))) {
+            System.out.println("Високосный год");
+        } else {
+            System.out.println("Невисокосный год");
+        }
     }
 
-    static int setInt(int integer) {
-        return integer;
+    /**
+     * Outputs a greeting to the console
+     */
+    static void outputGreeting(String name) {
+        System.out.println("Hello " + name + "!");
     }
 
-    static long setLong(long l) {
-        return l;
+    /**
+     * Checks the number on negative then returns result
+     */
+    static boolean isNegativeNumber(int number) {
+        if (number < 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    static float setFloat(Float f) {
-        return f;
+    /**
+     * Checks the number then outputs whether it is positive or negative number
+     */
+    static void positiveOrNegative(int number) {
+        if (number >= 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
     }
 
-    static double setDouble(double d) {
-        return d;
+    /**
+     * Checks numbers to see if they are in the range from 10 to 20 then returns result
+     */
+    static boolean isBetween10And20(int number_1, int number_2) {
+        if ((number_1 + number_2 >= 10) && (number_1 + number_2 <= 20)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    static char setChar(char c) {
-        return c;
+    /**
+     * Calculates an expression then returns result
+     */
+    static double calculate(float a, float b, float c, float d) {
+        return (a * (b + (c / d)));
     }
 
-    static boolean setBoolean(boolean logic) {
-        return logic;
-    }
 }
