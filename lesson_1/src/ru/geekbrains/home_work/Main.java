fa3eb09f8a894e8b8ac02816df2f5d5ecb400908
Temzu <40913z@mail.ru>
2020-08-31 18:24:23 +0300

    Update
    Added documentations for methods and corrected names of methods.

--- lesson_1/src/ru/geekbrains/home_work/Main.java
+++ lesson_1/src/ru/geekbrains/home_work/Main.java
@@ -3,23 +3,24 @@ package ru.geekbrains.home_work;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(calculate(1,2,3,4));
+        System.out.println(calculate(1, 2, 3, 4));
 
-        System.out.println(checkNumbers(2,18));
+        System.out.println(checkNumbers(2, 18));
 
         positiveOrNegative(5);
 
         System.out.println(isNegativeNumber(-1));
 
-        helloName("Artem");
+        outputGreeting("Artem");
 
-        isLeapYearOrNo(1600);
+        isLeapYearOrNot(1600);
 
         outputAllVariables((byte) 12, (short) 13, 125, 2312312312l, 12123.12f, 123213213.123123, 'A', true);
     }
 
-
-
+    /**
+     * Outputs all kinds of simple variables to the console.
+     */
     static void outputAllVariables(byte b, short s, int integer, long l, float f, double d, char c, boolean logic) {
         System.out.println("Переменная типа byte: " + b);
         System.out.println("Переменная типа short: " + s);
@@ -31,19 +32,28 @@ public class Main {
         System.out.println("Переменная типа boolean: " + logic);
     }
 
-    static void isLeapYearOrNo(int year) {
-        if (year % 4 == 0 && year % 100 != 0 || year % 4 == 0 && year % 400 == 0) {
+    /**
+     * Outputs to the console what year: leap year or not.
+     */
+    static void isLeapYearOrNot(int year) {
+        if (((year % 4 == 0) && (year % 100 != 0)) || ((year % 4 == 0) && (year % 400 == 0))) {
             System.out.println("Високосный год");
         } else {
             System.out.println("Невисокосный год");
         }
     }
 
-    static void helloName(String name) {
+    /**
+     * Outputs a greeting to the console
+     */
+    static void outputGreeting(String name) {
         System.out.println("Hello " + name + "!");
     }
 
-    static boolean isNegativeNumber (int number) {
+    /**
+     * Checks the number on negative then returns result
+     */
+    static boolean isNegativeNumber(int number) {
         if (number < 0) {
             return true;
         } else {
@@ -51,6 +61,9 @@ public class Main {
         }
     }
 
+    /**
+     * Checks the number then outputs whether it is positive or negative number
+     */
     static void positiveOrNegative(int number) {
         if (number >= 0) {
             System.out.println("Positive");
@@ -59,17 +72,22 @@ public class Main {
         }
     }
 
-    static boolean checkNumbers(int number_1, int number_2){
-        if (number_1 + number_2 >= 10 && number_1 + number_2 <= 20) {
+    /**
+     * Checks numbers to see if they are in the range from 10 to 20 then returns result
+     */
+    static boolean checkNumbers(int number_1, int number_2) {
+        if ((number_1 + number_2 >= 10) && (number_1 + number_2 <= 20)) {
             return true;
         } else {
             return false;
         }
     }
 
+    /**
+     * Calculates an expression then returns result
+     */
     static double calculate(float a, float b, float c, float d) {
-        return a * (b + (c / d));
+        return (a * (b + (c / d)));
     }
-
 
 }
