340df2a0276346536c2a756960fbcf50ec667413
Temzu <40913z@mail.ru>
2020-09-01 00:06:10 +0300

    Fourth task

--- lesson_2/src/ru/geekbrains/level_1/lesson_2/Main.java
+++ lesson_2/src/ru/geekbrains/level_1/lesson_2/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	    int[] arrayForFirstTask = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
 	    int[] arrayForSecondTask = new int[8];
 	    int[] arrayForThirdTask = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+	    int[][] arrayForFourthTask = new int[10][10];
 
         System.out.println("Задание 1");
         outputArrayToConsole(arrayForFirstTask);
@@ -20,15 +21,33 @@ public class Main {
         outputArrayToConsole(arrayForThirdTask);
         multiplyElementsOfAnArrayBy2(arrayForThirdTask);
         System.out.println();
+
+        System.out.println("Задание 4");
+        fillTheDiagonalElementsOfAnArray(arrayForFourthTask);
+        System.out.println();
     }
 
     /**
-     * Outputs all elements of the specified array.
+     * Outputs all elements of the specified one-dimensional array.
      */
     static void outputArrayToConsole(int[] array) {
         System.out.println("Выводим элементы массива");
-        for (int i: array) {
-            System.out.print(i + " ");
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Outputs all elements of the specified two-dimensional array.
+     */
+    static void outputTwoDimensionalArrayToConsole(int[][] array) {
+        System.out.println("Выводим элементы массива");
+        for (int[] i: array) {
+            for (int j: i) {
+                System.out.print(j + "  ");
+            }
+            System.out.println();
         }
         System.out.println();
     }
@@ -63,7 +82,7 @@ public class Main {
     }
 
     /** Third task
-     *  Multiplies all elements of an array less than 6 by 2
+     * Multiplies all elements of an array less than 6 by 2
      */
     static void multiplyElementsOfAnArrayBy2(int[] array) {
         for (int i = 0; i < array.length; i++) {
@@ -73,5 +92,20 @@ public class Main {
         }
         outputArrayToConsole(array);
     }
+
+    /** Forth task
+     * Fills the diagonal elements of a two-dimensional array with 1
+     */
+    static void fillTheDiagonalElementsOfAnArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i][i] = 1;
+        }
+        for (int j = array.length - 1; j >= 0; j--) {
+            array[j][array.length - j - 1] = 1;
+        }
+        outputTwoDimensionalArrayToConsole(array);
+    }
+
+
 
 }
